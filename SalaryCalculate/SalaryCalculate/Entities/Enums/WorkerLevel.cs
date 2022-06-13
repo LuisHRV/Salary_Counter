2a27157d7beb8b581d4bad0c548a9a399aeedf83
LuisHRV <luis.h01@hotmail.com>
2022-06-12 22:48:56 -0300

    Fix: Functions and Enums

--- SalaryCalculate/SalaryCalculate/Entities/Enums/WorkerLevel.cs
+++ SalaryCalculate/SalaryCalculate/Entities/Enums/WorkerLevel.cs
@@ -1,9 +1,9 @@
-﻿using System;
-
-namespace Salary.Calculate.Entities.Enums
+﻿namespace Salary.Calculate.Entities.Enums
 {
-    internal class WorkerLevel
+    enum WorkerLevel : int
     {
-
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2,
     }
 }
--- SalaryCalculate/SalaryCalculate/Entities/Worker.cs
+++ SalaryCalculate/SalaryCalculate/Entities/Worker.cs
@@ -1,9 +1,47 @@
-﻿using System;
+﻿using Salary.Calculate.Entities.Enums;
+using System.Collections.Generic;
 
 namespace Salary.Calculate.Entities
 {
-    internal class Worker
+    class Worker
     {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament Departament { get; set; }
+        public List<Contract> HourContract { get; set; } = new List<Contract>();
 
+        public Worker() { }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Departament departament)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+
+        public void AddContract(Contract hourContract)
+        {
+            HourContract.Add(hourContract);
+        }
+
+        public void RemoveContract ( Contract hourContract)
+        {
+            HourContract.Remove(hourContract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach(Contract hourContract in HourContract)
+            {
+                if (hourContract.Date.Year ==  year && hourContract.Date.Month == month)
+                {
+                    sum += hourContract.TotalValue();
+                }
+            }
+            return sum;
+        }
     }
 }
